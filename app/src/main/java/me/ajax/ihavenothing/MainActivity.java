c17a240e4225bac9395301b1d0fb9694e5783a4a
AJ <liaofuyou@yeah.net>
2018-05-09 16:49:48 +0800

    滚动效果

--- app/src/main/java/me/ajax/ihavenothing/MainActivity.java
+++ app/src/main/java/me/ajax/ihavenothing/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,46 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            int mAllOffsetY;
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                mAllOffsetY += dy;
+
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                for (int i = 0; i < recyclerView.getChildCount(); i++) {
+                    if (i != 0) {
+                        View view = recyclerView.getChildAt(i);
+                        if (view != null) {
+                            view.setScaleX(1F);
+                            view.setScaleY(1F);
+                        }
+                    }
+                }
+
+                View view = recyclerView.getChildAt(0);
+                if (view != null
+                        && recyclerView.getChildLayoutPosition(view) == layoutManager.findFirstVisibleItemPosition()) {
+
+                    float viewHeight = (float) view.getMeasuredHeight();
+                    float faction = mAllOffsetY % viewHeight / viewHeight;
+
+                    faction = 1 - 0.5f * faction;
+                    l(mAllOffsetY, dy, view.getMeasuredHeight(), faction, layoutManager.findFirstVisibleItemPosition());
+                    view.setScaleY(faction);
+                    view.setScaleX(faction);
+                }
+            }
+        });
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(cardAdapter = new CardAdapter());
     }
@@ -44,5 +86,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static void l(Object... list) {
+        StringBuilder text = new StringBuilder();
+        for (Object o : list) {
+            text.append("   ").append(o.toString());
+        }
+        Log.e("######", text.toString());
     }
 }
